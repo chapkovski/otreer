b2b9583d2a9699a3df706ec9f3f8ef0d9a176436
chapkovski <chapkovski@gmail.com>
2019-01-10 15:00:08 +0300

    extra doc for getting data

--- R/get_data.R
+++ R/get_data.R
@@ -1,15 +1,18 @@
 #' Getting data from oTree server
 #
-# This is the function that gets data from oTree-based server
-# which prints 'Hello, world!'.
-#
+#' This is the function that gets data from oTree-based server
+#' @section Url format:
+#' the url can be preceded or not by 'http' or 'https' prefix. If it is not provided
+#' it will be added automatically.
 #' @param website the http url to the website with the data
-# Some useful keyboard shortcuts for package authoring:
+#' @return data.frame with data in it
+
+#' @examples
+#' get_data('www.myotreeapp.herokuapp.com')
+
 #
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 #' @export
+
 
 get_data <- function(website) {
   print(paste("Getting data from:", website))
